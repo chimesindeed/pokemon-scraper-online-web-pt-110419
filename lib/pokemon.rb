2abc0f5ad300c656684b77e5e5feb1475e1aae30
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-04 20:37:59 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id:, name:, type:, db:)
+    @db = db
+    
+  end
+  
+  def self.save(name, type)
+   new_pokemon
+  end
 end
+
